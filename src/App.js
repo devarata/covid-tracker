bbb84a8ed45ac459c202ceafdacbb8589e71864c
Ride0401 <devarata.oza@gmail.com>
2020-07-25 18:00:06 +0530

    init project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
+    return (
+      <div className = "App">
 
-        <
-        /div>
+        </div>
     );
 }
 
