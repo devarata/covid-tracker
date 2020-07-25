216ee8fea1a90f3dac62503a1ad2ad0743ead84d
Ride0401 <devarata.oza@gmail.com>
2020-07-25 18:59:12 +0530

    drop down working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
 
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core"
+
+
 function App() {
-    return ( <div className = "App">
-      <h2>Covid19-tracker</h2>
+  const [countries,setCountries] = useState([
+    'USA','UK','India'
+  ]);
+
+
+    return (
+    <div className = "App">
+
+     <div className="app__header">
+      <h2>Covid19 tracker</h2>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map(country=> (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+
       {/*Header*/}
       {/*Title + select input dropdown*/}
 
@@ -16,8 +41,8 @@ function App() {
       {/*Graph*/}
 
       {/*Maps*/}
+   </div>
 
-    </div>
     );
 }
 
