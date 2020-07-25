9e8a2230151bdc559697e5a5682bdbc8772788c8
Ride0401 <devarata.oza@gmail.com>
2020-07-25 18:17:15 +0530

    website structure in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import './App.css';
 function App() {
     return ( <div className = "App">
       <h2>Covid19-tracker</h2>
+      {/*Header*/}
+      {/*Title + select input dropdown*/}
+
+      {/*Info boxes*/}
+      {/*Info boxes*/}
+      {/*Info boxes*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+
+      {/*Maps*/}
+
     </div>
     );
 }
